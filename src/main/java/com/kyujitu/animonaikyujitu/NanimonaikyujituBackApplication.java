42e74ed02df1e542b6583eacc63ca475157499e9
ymiru0324 <ymiru0324@naver.com>
2023-03-18 00:14:58 +0900

    IntelliJ - SpringBoot 연동

---
+++ src/main/java/com/kyujitu/animonaikyujitu/NanimonaikyujituBackApplication.java
@@ -0,0 +1,13 @@
+package com.kyujitu.animonaikyujitu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanimonaikyujituBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanimonaikyujituBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kyujitu/animonaikyujitu/NanimonaikyujituBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyujitu.animonaikyujitu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanimonaikyujituBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
