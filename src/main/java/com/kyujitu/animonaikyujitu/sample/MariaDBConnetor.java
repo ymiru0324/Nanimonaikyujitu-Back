3d45296bdb9ae2713616d7ccaf41700bc7faf1f0
ymiru0324 <ymiru0324@naver.com>
2023-03-19 18:52:46 +0900

    chore: MariaDB 연동 테스트

---
+++ src/main/java/com/kyujitu/animonaikyujitu/sample/MariaDBConnetor.java
@@ -0,0 +1,39 @@
+package com.kyujitu.animonaikyujitu.sample;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MariaDBConnetor {
+    public static void main(String[] args) {
+        Connection con = null;
+
+        String server = "localhost:3308";
+        String database = "nanimonaikyujitu";
+        String user_name = "root";
+        String password = "root";
+
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.err.println(" 드라이버 로딩 오류 : " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        try {
+            con = DriverManager.getConnection("jdbc:mariadb://" +
+                    server + "/" +
+                    database +
+                    "?useSSL=false", user_name, password); // SSL 실행 확인
+            System.out.println("연결 성공");
+        } catch(SQLException e) {
+            System.err.println("에러 내용 :" + e.getMessage());
+            e.printStackTrace();
+        }
+
+        try {
+            if(con != null)
+                con.close();
+        } catch (SQLException e) {}
+    }
+}
